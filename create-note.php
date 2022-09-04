d229639419156d72a60cba86ddb9e7468fc99290
caloy0714 <58654567+caloy0714@users.noreply.github.com>
2022-09-04 19:56:17 +0800

    Update create-note.php

--- create-note.php
+++ create-note.php
@@ -1,15 +1,18 @@
 <?php
+include "vendor/autoload.php";
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
 $client = new Client();
 $headers = [
   'Authorization' => 'NdQe03YgKb1pf9fMufhWVLtCBmMGloXs',
   'Content-Type' => 'application/json'
 ];
 $body = '{
-  "text": "caloy0714@gmail.com",
+  "text": "bernardino.carlitos@auf.edu.ph",
   "view_state": {
     "name": "public"
   }
 }';
-$request = new Request('POST', 'https://ipt10-2022.mantishub.io/api/rest/issues/[ISSUE_NUMBER]/notes', $headers, $body);
+$request = new Request('POST', 'https://ipt10-2022.mantishub.io/api/rest/issues/22131/notes', $headers, $body);
 $res = $client->sendAsync($request)->wait();
 echo $res->getBody();
